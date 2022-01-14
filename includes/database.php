f969d3bbb7160afdd0ec88933c034dba95786f39
JotaErrE01 <jruiz967@gmail.com>
2022-01-14 12:33:40 -0500

    agregando base de datos cloud

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,28 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'jotaerre01', 'tallerbd');
 
-
-if (!$db) {
+$db;
+try {
+    $db = mysqli_connect('bbcqvbjfufufhfhz3fwg-mysql.services.clever-cloud.com', 'utyohlj0zqsje2sx', 'gyZdu4BbTbYINhe4re1C', 'bbcqvbjfufufhfhz3fwg');
+    if(!$db) {
+        throw new Exception('No se pudo conectar a la base de datos');
+    }
+} catch (\Throwable $th) {
+    echo $th;
     echo "Error: No se pudo conectar a MySQL.";
     echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
 }
+
+
+
+// $db = mysqli_connect('localhost', 'root', 'jotaerre01', 'tallerbd');
+
+
+// if (!$db) {
+//     echo "Error: No se pudo conectar a MySQL.";
+//     echo "errno de depuración: " . mysqli_connect_errno();
+//     echo "error de depuración: " . mysqli_connect_error();
+//     exit;
+// }
