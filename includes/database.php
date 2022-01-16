508514533867004873cb8cdbb1d7ba29322c71ac
JotaErrE01 <jruiz967@gmail.com>
2022-01-16 18:10:39 -0500

    ajustando variables de entorno

--- includes/database.php
+++ includes/database.php
@@ -2,10 +2,7 @@
 
 $db;
 try {
-
-    $db = mysqli_connect('localhost', 'root', 'jotaerre01', 'tallerbd');
-
-    // $db = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"]);
+    $db = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"]);
     if(!$db) {
         throw new Exception('No se pudo conectar a la base de datos');
     }
